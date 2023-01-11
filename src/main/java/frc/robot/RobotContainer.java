5cf55e1838f44b406fc9c0b2cf0935d02db67a60
SamFelsted <sam.felsted@icloud.com>
2023-01-11 09:11:57 -0800

    4 spaces

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,52 +13,52 @@ import frc.robot.subsystems.SwerveDrive;
 
 
 public class RobotContainer {
-  private final SwerveDrive m_swerveDrive = new SwerveDrive();
-  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    private final SwerveDrive m_swerveDrive = new SwerveDrive();
+    private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-  public RobotContainer() {
+    public RobotContainer() {
 
-    m_swerveDrive.setDefaultCommand(new DefaultDriveCommand(
-            m_swerveDrive,
-            () -> -square(modifyAxis(m_driverController.getLeftY()) * m_swerveDrive.getMaxSpeed()),
-            () -> -square(modifyAxis(m_driverController.getLeftX()) * m_swerveDrive.getMaxSpeed()),
-            () -> -square(modifyAxis(m_driverController.getRightX()) * m_swerveDrive.getMaxSpeed())
-    ));
-    configureBindings();
-  }
-
-  private void configureBindings() {
-
-    m_driverController.b().whileTrue(m_swerveDrive.runOnce(m_swerveDrive::zeroGyroscope));
-  }
-
-
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return AutoFactory.getSquareAuto(m_swerveDrive);
-  }
-
-  private static double deadband(double value, double deadband) {
-    if (Math.abs(value) > deadband) {
-      if (value > 0.0) {
-        return (value - deadband) / (1.0 - deadband);
-      } else {
-        return (value + deadband) / (1.0 - deadband);
-      }
-    } else {
-      return 0.0;
+        m_swerveDrive.setDefaultCommand(new DefaultDriveCommand(
+                m_swerveDrive,
+                () -> -square(modifyAxis(m_driverController.getLeftY()) * m_swerveDrive.getMaxSpeed()),
+                () -> -square(modifyAxis(m_driverController.getLeftX()) * m_swerveDrive.getMaxSpeed()),
+                () -> -square(modifyAxis(m_driverController.getRightX()) * m_swerveDrive.getMaxSpeed())
+        ));
+        configureBindings();
     }
-  }
 
-  private static double modifyAxis(double value) {
-    // Deadband
-    value = deadband(value, 0.075);
-    // Square the axis
-    value = Math.copySign(value * value, value);
-    return value;
-  }
+    private void configureBindings() {
 
-  private static double square(double value) {
-    return Math.copySign(value * value, value);
-  }
+        m_driverController.b().whileTrue(m_swerveDrive.runOnce(m_swerveDrive::zeroGyroscope));
+    }
+
+
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        return AutoFactory.getSquareAuto(m_swerveDrive);
+    }
+
+    private static double deadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                return (value - deadband) / (1.0 - deadband);
+            } else {
+                return (value + deadband) / (1.0 - deadband);
+            }
+        } else {
+            return 0.0;
+        }
+    }
+
+    private static double modifyAxis(double value) {
+        // Deadband
+        value = deadband(value, 0.075);
+        // Square the axis
+        value = Math.copySign(value * value, value);
+        return value;
+    }
+
+    private static double square(double value) {
+        return Math.copySign(value * value, value);
+    }
 }
