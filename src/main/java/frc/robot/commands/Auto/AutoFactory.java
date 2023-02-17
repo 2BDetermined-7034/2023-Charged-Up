895d564ab32ffa52317633886afb82e03136f869
Team7034 <33676445+Team7034@users.noreply.github.com>
2023-02-16 18:58:32 -0800

    refactors - need to test

--- src/main/java/frc/robot/commands/Auto/AutoFactory.java
+++ src/main/java/frc/robot/commands/Auto/AutoFactory.java
@@ -3,19 +3,16 @@ package frc.robot.commands.Auto;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.AutoBalance;
 import frc.robot.commands.Drive.PathFactory;
-import frc.robot.constants.Constants;
 import frc.robot.subsystems.Arm.Arm;
 import frc.robot.subsystems.SwerveDrive;
 import frc.robot.subsystems.VisionLocking;
 
-import java.util.HashMap;
-
 public class AutoFactory {
-    static HashMap<String, Command> eventMap = new HashMap<>();
     SendableChooser<Integer> startingPosition;
     AutoAction firstAction;
     AutoAction secondAction;
@@ -42,8 +39,28 @@ public class AutoFactory {
         secondAction = new AutoAction("First", m_drive, m_arm);
     }
     public Command getAuto() {
-        Command first = firstAction.getSelected(m_visionlocker.getTeam(), startingPosition.getSelected());
+        int startingPos = startingPosition.getSelected();
+        switch (startingPos) {
+            case 1:
+                m_drive.setPosition(new Pose2d(2, 4.5, new Rotation2d()));
+                break;
+            case 2:
+                m_drive.setPosition(new Pose2d(2, 2.8, new Rotation2d()));
+                break;
+            case 3:
+                m_drive.setPosition(new Pose2d(2, 1, new Rotation2d()));
+                break;
+            default:
+                break;
+        }
 
-        return first;
+        return firstAction.getSelected(m_visionlocker.getTeam(), startingPos);
+    }
+
+
+    static Command getTwoPiece(SwerveDrive m_drive) {
+        PathPlannerTrajectory path = PathPlanner.loadPath("", new PathConstraints(2, 3));
+
+        return new PathFactory(m_drive, path, true, true).getCommand();
     }
 }
