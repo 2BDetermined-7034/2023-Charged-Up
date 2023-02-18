0444f8ef5b90d83713d942f976c70695c7829049
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-02-17 17:34:18 -0800

    15

--- src/main/java/frc/robot/subsystems/GravityClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/GravityClawSubsystem.java
@@ -6,23 +6,27 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 
-public class GravityClawSubsystem extends SubsystemBase implements SubsystemLogging{
-    private DoubleSolenoid sol;
+public class GravityClawSubsystem extends SubsystemBase implements SubsystemLogging {
+    private final DoubleSolenoid sol;
+
     public GravityClawSubsystem() {
-        sol = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.GravityClaw.forwardChannel,Constants.GravityClaw.reverseChannel);
+        sol = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.GravityClaw.forwardChannel, Constants.GravityClaw.reverseChannel);
         // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
         //       in the constructor or in the robot coordination class, such as RobotContainer.
         //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
         //       such as SpeedControllers, Encoders, DigitalInputs, etc.
     }
-    public void setSolonoid(boolean direction){
-        if(direction){
-        sol.set(DoubleSolenoid.Value.kReverse);
+
+    public void setSolonoid(boolean direction) {
+        if (direction) {
+            sol.set(DoubleSolenoid.Value.kReverse);
         } else sol.set(DoubleSolenoid.Value.kForward);
     }
+
     public DoubleSolenoid.Value getSolenoid() {
         return sol.get();
     }
+
     public void ToggleSolonoid() {
         if (sol.get().equals(DoubleSolenoid.Value.kForward)) {
             sol.set(DoubleSolenoid.Value.kReverse);
@@ -30,6 +34,7 @@ public class GravityClawSubsystem extends SubsystemBase implements SubsystemLogg
             sol.set(DoubleSolenoid.Value.kForward);
         }
     }
+
     @Override
     public void periodic() {
         updateLogging();
