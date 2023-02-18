6163bfceae1b5b2ec44cd1288dc152ef7e48bbfe
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-02-17 17:36:40 -0800

    21

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,16 +9,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import frc.robot.commands.Arm.ArmOverride;
 import frc.robot.commands.Arm.SetArmCommand;
-
+import frc.robot.commands.Auto.AutoFactory;
 import frc.robot.commands.Drive.AutoBalance;
 import frc.robot.commands.Drive.DefaultDriveCommand;
 import frc.robot.commands.clob.GravityClawCommand;
 import frc.robot.commands.clob.GravityClawToggleCommand;
 import frc.robot.constants.Constants.OperatorConstants;
-import frc.robot.commands.Auto.AutoFactory;
 import frc.robot.subsystems.Arm.Arm;
 import frc.robot.subsystems.GravityClawSubsystem;
-
 import frc.robot.subsystems.SwerveDrive;
 import frc.robot.subsystems.VisionLocking;
 
@@ -40,16 +38,39 @@ public class RobotContainer {
     private final AutoBalance balance = new AutoBalance(m_swerveDrive);
 
 
-
     public RobotContainer() {
 
         m_swerveDrive.setDefaultCommand(new DefaultDriveCommand(
                 m_swerveDrive,
-                () -> -square(modifyAxis(m_driverController.getLeftY() ) * m_swerveDrive.getMaxSpeed()),
+                () -> -square(modifyAxis(m_driverController.getLeftY()) * m_swerveDrive.getMaxSpeed()),
                 () -> -square(modifyAxis(m_driverController.getLeftX()) * m_swerveDrive.getMaxSpeed()),
                 () -> -square(modifyAxis(m_driverController.getRightX()) * m_swerveDrive.getMaxSpeed())
         ));
         configureBindings();
+    }
+
+    private static double deadband(double value, double deadband) {
+        if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                return (value - deadband) / (1.0 - deadband);
+            } else {
+                return (value + deadband) / (1.0 - deadband);
+            }
+        } else {
+            return 0.0;
+        }
+    }
+
+    private static double modifyAxis(double value) {
+        // Deadband
+        value = deadband(value, 0.031);
+        // Square the axis
+        value = Math.copySign(value * value, value);
+        return value;
+    }
+
+    private static double square(double value) {
+        return Math.copySign(value * value, value);
     }
 
     private void configureBindings() {
@@ -63,7 +84,7 @@ public class RobotContainer {
 
         m_driverController.triangle().onTrue(new SetArmCommand(m_Arm, Units.degreesToRadians(90), Units.degreesToRadians(90)));
         m_driverController.square().onTrue(new SetArmCommand(m_Arm, Units.degreesToRadians(90), Units.degreesToRadians(309)));
-        m_driverController.circle().onTrue(new SetArmCommand(m_Arm,Units.degreesToRadians(130),  Units.degreesToRadians(45)));
+        m_driverController.circle().onTrue(new SetArmCommand(m_Arm, Units.degreesToRadians(130), Units.degreesToRadians(45)));
         m_driverController.cross().onTrue(new SetArmCommand(m_Arm, Units.degreesToRadians(90), Units.degreesToRadians(270)));
 
         m_operatorController.circle().onTrue(gravityClawCommandTrue);
@@ -94,35 +115,10 @@ public class RobotContainer {
 
     }
 
-
     public Command getAutonomousCommand() {
         // An example command will be run in autonomous
         return AutoFactory.getSmallSquare(m_swerveDrive);
 
-    }
-
-    private static double deadband(double value, double deadband) {
-        if (Math.abs(value) > deadband) {
-            if (value > 0.0) {
-                return (value - deadband) / (1.0 - deadband);
-            } else {
-                return (value + deadband) / (1.0 - deadband);
-            }
-        } else {
-            return 0.0;
-        }
-    }
-
-    private static double modifyAxis(double value) {
-        // Deadband
-        value = deadband(value, 0.031);
-        // Square the axis
-        value = Math.copySign(value * value, value);
-        return value;
-    }
-
-    private static double square(double value) {
-        return Math.copySign(value * value, value);
     }
 
 
