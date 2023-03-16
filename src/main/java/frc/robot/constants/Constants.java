15219d8670f04b7480530eef594477addfbf98c8
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-03-15 17:23:20 -0700

    boowomp I'm coding remotely while having someone on a discord call at home

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
         public static final int grabberFC = 10;
         public static final int grabberRC = 11;
 
-        public static final int activeGrabberMotor = 23;
+        public static final int activeGrabberMotor = 13;
     }
 
     public static class Drivebase {
