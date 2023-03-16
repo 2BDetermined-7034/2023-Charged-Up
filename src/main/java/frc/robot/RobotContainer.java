30c06d2c46109dbc0d94b136260169439aa50d8d
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-03-15 18:36:18 -0700

    boowomp I'm coding remotely while having someone on a discord call at home

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -136,9 +136,7 @@ public class RobotContainer {
        new Trigger(m_operatorController::getRightBumper).whileTrue(m_visionLocker.runOnce(m_visionLocker::gridRight));
 
 //       new Trigger(m_operatorController::getBackButton).onTrue(new GravityClawToggleCommand(gravityClawSubsystem));
-
-
-        new Trigger(m_operatorController::getAButton).whileTrue(new ActiveGrabberMotor(ags, 0.5));
+        new JoystickButton(m_operatorController, 1).whileTrue(new ActiveGrabberMotor(ags, 0.5));;
 //        new Trigger(m_operatorController::getAButton).onTrue(ArmPathFactory.getIntakePath(m_Arm, gravityClawSubsystem, intake)); // high// med
 //        new Trigger(m_operatorController::getBButton).onTrue(ArmPathFactory.getScoreMidPath(m_Arm, intake)); // low
 //        new Trigger(m_operatorController::getYButton).onTrue(ArmPathFactory.getScoreHighPath(m_Arm, intake)); // low
